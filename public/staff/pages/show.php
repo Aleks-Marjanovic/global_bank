6f56a36ba8e5de169fbd6300b12094cfb3ececdb
Aleks Marjanovic <alex.marjanovic11@gmail.com>
2021-03-11 22:23:05 -0500

    Completed Chapter 2 Challenge. Next is Chapter 3

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -4,13 +4,14 @@
   $id = $_GET['id'] ?? '1';
 ?>
 
-<?php $page_title = 'Show Subject'; ?>
+<?php $page_title = 'Show Page'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
-  <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
-  <div class="subject show">
-    Subject ID: <?php echo h($id); ?>
+  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="page show">
+    Page ID: <?php echo h($id); ?>
   </div>
 </div>
 
