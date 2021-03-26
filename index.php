c3f0f38b9de882ad40462d3c689cd6951dde6f23
Aleks Marjanovic <alex.marjanovic11@gmail.com>
2021-03-25 21:48:51 -0400

    Adds index.php to the landing page of web182 subdomain

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB 182</title>
+  </head>
+
+  <body>
+    <h1>WEB 182</h1>
+    <p><a href="globe_bank/public/staff">Globe Bank (staff)</a></p>
+  </body>
+</html>
